324e30073869739a64b54b072aca9822edf7b758
Serge Poltavski <serge.poltavski@gmail.com>
2019-06-04 12:05:11 +0300

    change to enums

--- constants.h
+++ constants.h
@@ -4,26 +4,29 @@ namespace alpaca {
 
 const byte I2C_ADDR = 0x60;
 
-const byte CMD_START = 0x81;
-const byte CMD_SEND_DIGITAL = 0x90;
-const byte CMD_SEND_ANALOG = 0xA0;
-const byte CMD_SEND_ANALOG_RAW = 0xB0;
-const byte CMD_RESPONSE = 0xC0;
-const byte CMD_END = 0x80;
-
-const byte CMD_TARGET = 0xD0;
-const byte CMD_TARGET_JACK0 = 0x0;
-const byte CMD_TARGET_JACK1 = 0x1;
-const byte CMD_TARGET_JACK_BOTH = 0x2;
-const byte CMD_TARGET_MATRIX = 0x3;
-const byte CMD_JACK_SET_MODE = 0x10;
-const byte CMD_MATRIX_SET_BRIGHTNESS = 0x10;
-const byte CMD_MATRIX_SET_PIXEL = 0x20;
-const byte CMD_MATRIX_CLEAR_PIXEL = 0x30;
-const byte CMD_MATRIX_CLEAR = 0x40;
-const byte CMD_MATRIX_FILL = 0x50;
-const byte CMD_MATRIX_DRAW = 0x60;
-const byte CMD_MATRIX_CHAR = 0x70;
+enum CommandType {
+  CMD_START = 0x81,
+  CMD_END = 0x80,
+  // out
+  CMD_SEND_DIGITAL = 0x90,
+  CMD_SEND_ANALOG = 0xA0,
+  CMD_SEND_ANALOG_RAW = 0xB0,
+  CMD_RESPONSE = 0xC0,
+  // in
+  CMD_TARGET = 0xD0,
+  CMD_TARGET_JACK0 = 0x0,
+  CMD_TARGET_JACK1 = 0x1,
+  CMD_TARGET_JACK_BOTH = 0x2,
+  CMD_TARGET_MATRIX = 0x3,
+  CMD_JACK_SET_MODE = 0x10,
+  CMD_MATRIX_SET_BRIGHTNESS = 0x10,
+  CMD_MATRIX_SET_PIXEL = 0x20,
+  CMD_MATRIX_CLEAR_PIXEL = 0x30,
+  CMD_MATRIX_CLEAR = 0x40,
+  CMD_MATRIX_FILL = 0x50,
+  CMD_MATRIX_DRAW = 0x60,
+  CMD_MATRIX_CHAR = 0x70
+};
 
 enum JackMode {
   MODE_NONE = 0,
